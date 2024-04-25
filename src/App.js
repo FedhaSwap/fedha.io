2edb3bc20231793da8b6cbd29b214f0afea3db4d
Evans Mburu <e.mburu@centum.co.ke>
2024-04-25 18:45:17 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import Home from './pages/website/home'
 import About from './pages/website/about'
 import Swap from './pages/account/swap'
 import OrderlyApi from './pages/website/orderlyApi'
-import Header from '../src/pages/components/header'
-import Footer from '../src/pages/components/footer'
+
 import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
 
 
